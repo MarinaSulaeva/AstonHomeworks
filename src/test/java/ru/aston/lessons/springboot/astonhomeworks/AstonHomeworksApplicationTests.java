a4f81d9b88709d8b932c7cd63cf948d83a7a33f4
SulaevaMV <sulaevamv@gmail.com>
2024-11-27 21:35:06 +0500

    initial commit

---
+++ src/test/java/ru/aston/lessons/springboot/astonhomeworks/AstonHomeworksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.lessons.springboot.astonhomeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonHomeworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
